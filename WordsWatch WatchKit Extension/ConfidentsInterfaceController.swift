8b753073412042232ed18a2ff809ebdade371483
div-antk <51283468+owaii@users.noreply.github.com>
2022-04-02 08:19:13 +0900

    自信あるリストのコントローラを作成

---
+++ WordsWatch WatchKit Extension/ConfidentsInterfaceController.swift
@@ -0,0 +1,16 @@
+//
+//  ConfidentsInterfaceController.swift
+//  WordsWatch WatchKit Extension
+//
+//  Created by Takuya Ando on 2022/04/02.
+//
+
+import UIKit
+import WatchKit
+
+class ConfidentsInterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+    
+    }
+}
