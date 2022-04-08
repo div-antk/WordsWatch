45d89e1f616474b5c7326409a67d4fa7159770bc
div-antk <51283468+owaii@users.noreply.github.com>
2022-04-08 21:42:37 +0900

    行のコントローラを作成し、Labelを紐付ける

---
+++ WordsWatch WatchKit Extension/WordRowController.swift
@@ -0,0 +1,13 @@
+//
+//  WordRowController.swift
+//  WordsWatch WatchKit Extension
+//
+//  Created by Takuya Ando on 2022/04/08.
+//
+
+import WatchKit
+
+class WordRowController: NSObject {
+    
+    @IBOutlet weak var wordLabel: WKInterfaceLabel!
+}
