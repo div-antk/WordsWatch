a3f2abf4d9dd31a47342f90437c2e6d64a6ad1d4
div-antk <51283468+owaii@users.noreply.github.com>
2022-04-12 23:06:57 +0900

    リストに値を設定するテスト

--- WordsWatch WatchKit Extension/ConfidentsInterfaceController.swift
+++ WordsWatch WatchKit Extension/ConfidentsInterfaceController.swift
@@ -11,13 +11,20 @@ class ConfidentsInterfaceController: WKInterfaceController {
 
     @IBOutlet weak var confidentsTable: WKInterfaceTable!
     
-    let words = ["aaa", "bbb", "ccc"]
     var confidents:[String] = []
+    var items = ["name" : "udon"]
     
     override func awake(withContext context: Any?) {
         
         confidents = context as! [String]
         // TableViewのIdentitfierを指定
         confidentsTable.setNumberOfRows(confidents.count, withRowType: "MainRowType")
+        
+        for(index, item) in items.enumerated() {
+            let row = confidentsTable.rowController(at: index) as! WordRowController
+            row.wordLabel.setText(item.value)
+
+//            row.wordLabel.setText(item)
+        }
     }
 }
